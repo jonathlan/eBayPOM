8a7e4fb2d2ddf2976fd52608df1fa645510efaf1
Jonathan Solis <38994980+sacrofer@users.noreply.github.com>
2018-07-23 22:06:43 -0500

    Call properties from external file

--- src/helpers/CustomProperties.java
+++ src/helpers/CustomProperties.java
@@ -6,17 +6,8 @@ import java.io.IOException;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
-public class CustomProperties {	
-	/*Driver's properties*/
-	public static final String GENERIC_DRIVER_LOCATION = "C:\\testautomation\\libs\\drivers\\";
-	public static final String CHROME_DRIVER_NAME = "webdriver.chrome.driver";
-	public static final String CHROME_DRIVER_LOCATION = GENERIC_DRIVER_LOCATION + "chromedriver.exe";
-	public static final String FIREFOX_DRIVER_NAME = "webdriver.gecko.driver";
-	public static final String FIREFOX_DRIVER_LOCATION = GENERIC_DRIVER_LOCATION + "geckodriver.exe";
-	
-	/*WebPage URL*/
-	public static final String MAIN_URL = "https://www.ebay.com/";
-	
+public class CustomProperties {		
+		
 	public Properties LoadProperties(String xmlFile) throws InvalidPropertiesFormatException, FileNotFoundException, IOException {
 		Properties properties = new Properties();
 		properties.loadFromXML(new FileInputStream(xmlFile));
