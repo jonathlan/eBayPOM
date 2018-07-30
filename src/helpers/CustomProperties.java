6ba485982923247d36ca31cb6bcb24f7d9635e04
Jonathan Solis <38994980+sacrofer@users.noreply.github.com>
2018-07-29 19:28:24 -0500

    Add javadoc information for loadFromJSon and LoadProperties methods

--- src/helpers/CustomProperties.java
+++ src/helpers/CustomProperties.java
@@ -13,6 +13,19 @@ import com.eclipsesource.json.ParseException;
 
 public class CustomProperties {		
 	
+	/**
+	   * Loads a XML properties object from the given file.
+	   *
+	   * @param xmlFile
+	   *          the location to the XML file as a String
+	   * @return the properties object that has been loaded
+	   * @throws InvalidPropertiesFormatException
+	   *           if the file does not meet java properties format
+	   * @throws FileNotFoundException
+	   *           if the file cannot be located
+	   * @throws IOException
+	   *           if an I/O error occurs in the reader 
+	   */
 	public Properties LoadProperties(String xmlFile)  {
 		try {
 			
@@ -30,6 +43,21 @@ public class CustomProperties {
 		return null;
 	}	
 	
+	/**
+	   * Loads a JSON object from the given file.
+	   *
+	   * @param file
+	   *          the location to the file as a String
+	   * @return the JSON object that has been loaded
+	   * @throws FileNotFoundException
+	   *           if the file cannot be located
+	   * @throws IOException
+	   *           if an I/O error occurs in the reader
+	   * @throws ParseException
+	   *           if the input is not valid JSON
+	   * @throws UnsupportedOperationException
+	   *           if the input does not contain a JSON object	   * 
+	   */
 	public JsonObject loadFromJSon(String file) {
 		
 		try {
