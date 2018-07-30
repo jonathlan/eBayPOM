f1e988c444e4e324afb83d05fa74c4a57bd14bb2
Jonathan Solis <38994980+sacrofer@users.noreply.github.com>
2018-07-29 19:24:01 -0500

    Add loadFromJSon(String file) method in order to support reading properties from a JSON file
    JSON file is parsed and values are obtained  using minimal-json, for more information go to: https://github.com/ralfstx/minimal-json

--- src/helpers/CustomProperties.java
+++ src/helpers/CustomProperties.java
@@ -6,11 +6,47 @@ import java.io.IOException;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.ParseException;
+
+
 public class CustomProperties {		
+	
+	public Properties LoadProperties(String xmlFile)  {
+		try {
+			
+			Properties properties = new Properties();
+			properties.loadFromXML(new FileInputStream(xmlFile));
+			return properties;
+			
+		} catch (InvalidPropertiesFormatException e) {
+			e.printStackTrace();
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}	
+	
+	public JsonObject loadFromJSon(String file) {
 		
-	public Properties LoadProperties(String xmlFile) throws InvalidPropertiesFormatException, FileNotFoundException, IOException {
-		Properties properties = new Properties();
-		properties.loadFromXML(new FileInputStream(xmlFile));
-		return properties;
+		try {
+			
+			java.io.FileReader rFile = new java.io.FileReader(file);			
+			JsonObject jObject = Json.parse(rFile).asObject();
+			return jObject;
+			
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}catch (ParseException e) {
+			e.printStackTrace();
+		}catch (UnsupportedOperationException e) {
+			e.printStackTrace();
+		}
+		return null;	      
 	}
 }
