080c9345b629b9a1beb6026506e90a4609ec25a6
Jonathan Solis <38994980+sacrofer@users.noreply.github.com>
2018-08-01 20:37:39 -0500

    Add locators for language selection and results page

--- src/helpers/Locators.java
+++ src/helpers/Locators.java
@@ -1,6 +1,14 @@
 package helpers;
 
 public class Locators {
+	/*Home page*/
 	public static final String SEARCH_BTN_XPATH = "//input[@class= 'btn btn-prim gh-spr']";
 	public static final String SEARCH_TXT_XPATH = "//input[contains(@class, 'gh-tb ui-autocomplete-input')]";
+	public static final String GEO_LINK_XPATH = "//a[@id='gh-eb-Geo-a-default']";
+	public static final String CURR_LANGUAGE_XPATH = "//a[@id='gh-eb-Geo-a-default']/Span[1]";
+	public static final String EN_LINK_XPATH = "//li[@lang='en-US']";
+	
+	/*Search results page*/
+	public static final String RESULTS_TBL_ID = "srp-river-results";
+	
 }
