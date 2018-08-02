b116d4f771c8b800390417928e93f53b1fbd1555
Jonathan Solis <38994980+sacrofer@users.noreply.github.com>
2018-08-01 20:43:49 -0500

    Select the language before test starts and assert that the article is actually found

--- src/com/ebay/test/EbayTests.java
+++ src/com/ebay/test/EbayTests.java
@@ -1,5 +1,7 @@
 package com.ebay.test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,7 +15,9 @@ public class EbayTests extends EbayTestBasePOM {
 	@FileParameters("./data/ebayparams.csv")
 	public void test1(String browser, String article) {
 		setUpPhase2(browser, jProp.get("MAIN_URL").asString());
+		eHomePage.setLanguage();
 		eHomePage.searchArticle(article);
+		assertTrue("Article not found", eSearchResultsPage.isArticleFound(article));
 	}
 
 }
